967ffb3ff8b2f3ce0e318a8759e2954baa93b1f1
Lincoln Wisely <lwisely@hlkagency.com>
2021-01-06 21:09:59 -0600

    add initial files

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return(
+      <div>
+        <h1>We're making an app! We're making an app! We're making an app!</h1>
+        <h2>I never really know what to do. I need to figure out what to do. Maybe use Ant Design web components. </h2>
+      </div>
+    )
+  }
+}
+
+export default App;
